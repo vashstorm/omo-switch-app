08f1ac83e8c523539a4f4b11d8ec2bc35e1b6721
vashstorm <vashstorm@gmail.com>
2026-05-13 18:39:54 +0800

    chore(build): 更新嵌入资源引用
    - 同步前端构建产物 hash 变化后的资源映射
    - 更新 SyncReplacePreviewDialog、RawConfigDialog、ErrorLogPanel 等 chunk 引用

--- src/server/embedded-assets.ts
+++ src/server/embedded-assets.ts
@@ -1,17 +1,17 @@
 // @ts-nocheck
 // Auto-generated file - do not edit manually
+import asset_SyncReplacePreviewDialog_7wctsyy3_js from "../../dist/web/SyncReplacePreviewDialog-7wctsyy3.js" with { type: "file" };
+import asset_RawConfigDialog_59dtrb9s_js from "../../dist/web/RawConfigDialog-59dtrb9s.js" with { type: "file" };
 import asset_index_html from "../../dist/web/index.html" with { type: "file" };
-import asset_main_tepcdnv5_js from "../../dist/web/main-tepcdnv5.js" with { type: "file" };
+import asset_main_ecr994hh_js from "../../dist/web/main-ecr994hh.js" with { type: "file" };
 import asset_main_j5w3aanp_js from "../../dist/web/main-j5w3aanp.js" with { type: "file" };
 import asset_icon_svg from "../../dist/web/icon.svg" with { type: "file" };
 import asset_index_js from "../../dist/web/index.js" with { type: "file" };
-import asset_RawConfigDialog_hh8ex5yn_js from "../../dist/web/RawConfigDialog-hh8ex5yn.js" with { type: "file" };
-import asset_main_w02fskqr_js from "../../dist/web/main-w02fskqr.js" with { type: "file" };
-import asset_SyncReplacePreviewDialog_vyq7wbva_js from "../../dist/web/SyncReplacePreviewDialog-vyq7wbva.js" with { type: "file" };
-import asset_ErrorLogPanel_gdyk5q7x_js from "../../dist/web/ErrorLogPanel-gdyk5q7x.js" with { type: "file" };
+import asset_ErrorLogPanel_b7s708a3_js from "../../dist/web/ErrorLogPanel-b7s708a3.js" with { type: "file" };
 import asset_main_rt7mbnwb_js from "../../dist/web/main-rt7mbnwb.js" with { type: "file" };
 import asset_webview_22r21d1n_js from "../../dist/web/webview-22r21d1n.js" with { type: "file" };
 import asset_main_4p8k03ft_js from "../../dist/web/main-4p8k03ft.js" with { type: "file" };
+import asset_main_p7h3yeb9_js from "../../dist/web/main-p7h3yeb9.js" with { type: "file" };
 import asset_poppins_700_woff2 from "../../dist/web/fonts/poppins-700.woff2" with { type: "file" };
 import asset_ibm_plex_mono_700_woff2 from "../../dist/web/fonts/ibm-plex-mono-700.woff2" with { type: "file" };
 import asset_poppins_600_woff2 from "../../dist/web/fonts/poppins-600.woff2" with { type: "file" };
@@ -21,18 +21,18 @@ import asset_poppins_500_woff2 from "../../dist/web/fonts/poppins-500.woff2" wit
 import asset_ibm_plex_mono_400_woff2 from "../../dist/web/fonts/ibm-plex-mono-400.woff2" with { type: "file" };
 
 export const embeddedAssets: Record<string, string> = {
+  "/SyncReplacePreviewDialog-7wctsyy3.js": asset_SyncReplacePreviewDialog_7wctsyy3_js,
+  "/RawConfigDialog-59dtrb9s.js": asset_RawConfigDialog_59dtrb9s_js,
   "/index.html": asset_index_html,
-  "/main-tepcdnv5.js": asset_main_tepcdnv5_js,
+  "/main-ecr994hh.js": asset_main_ecr994hh_js,
   "/main-j5w3aanp.js": asset_main_j5w3aanp_js,
   "/icon.svg": asset_icon_svg,
   "/index.js": asset_index_js,
-  "/RawConfigDialog-hh8ex5yn.js": asset_RawConfigDialog_hh8ex5yn_js,
-  "/main-w02fskqr.js": asset_main_w02fskqr_js,
-  "/SyncReplacePreviewDialog-vyq7wbva.js": asset_SyncReplacePreviewDialog_vyq7wbva_js,
-  "/ErrorLogPanel-gdyk5q7x.js": asset_ErrorLogPanel_gdyk5q7x_js,
+  "/ErrorLogPanel-b7s708a3.js": asset_ErrorLogPanel_b7s708a3_js,
   "/main-rt7mbnwb.js": asset_main_rt7mbnwb_js,
   "/webview-22r21d1n.js": asset_webview_22r21d1n_js,
   "/main-4p8k03ft.js": asset_main_4p8k03ft_js,
+  "/main-p7h3yeb9.js": asset_main_p7h3yeb9_js,
   "/fonts/poppins-700.woff2": asset_poppins_700_woff2,
   "/fonts/ibm-plex-mono-700.woff2": asset_ibm_plex_mono_700_woff2,
   "/fonts/poppins-600.woff2": asset_poppins_600_woff2,
